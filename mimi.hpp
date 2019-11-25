0bc13b69af86800fbf9fa7d38f536979487afd2c
Nicholas Warfield <warfield9000@gmail.com>
2019-11-24 20:35:16 -0800

    Fixed some pointer bugs Should probably avoid using pointers until I get everything working without them.

--- mimi.hpp
+++ mimi.hpp
@@ -11,7 +11,7 @@ namespace mimi
 	class iterator : public std::iterator<std::forward_iterator_tag, A>
 	{
 		public:
-			virtual std::unique_ptr<A> next(void) = 0;
+			virtual A next(void) = 0;
 			virtual bool hasNext(void) = 0;
 	};
 
@@ -19,7 +19,7 @@ namespace mimi
 	class mzero : public iterator<A>
 	{
 		public:
-			std::unique_ptr<A> next(void) { return nullptr; }
+			A next(void) { return A(); }
 			bool hasNext(void) { return false; }
 	};
 
@@ -28,8 +28,8 @@ namespace mimi
 	{
 		public:
 			unit(A item) : m_item(std::make_unique<A>(item)) { }
-			std::unique_ptr<A> next(void) { return m_item.release(); }
-			bool hasNext(void) { return m_item; }
+			A next(void) { return *(m_item.release()); }
+			bool hasNext(void) { return (bool)m_item; }
 
 		private:
 			std::unique_ptr<A> m_item;
@@ -44,14 +44,15 @@ namespace mimi
 				m_a(std::move(a)),
 				m_f(f)
 			{
-				m_b = std::make_unique<iterator<B>>
-					(m_a->hasNext() ? m_f( *(m_a->next()) ) : mzero<B>());
+				if (m_a->hasNext())
+				{ m_b = std::move(m_f(m_a->next())); }
+				else { m_b = std::make_unique<mzero<B>>(); }
 			}
 
-			std::unique_ptr<A> next(void)
+			A next(void)
 			{
 				if (m_b->hasNext()) { return m_b->next(); }
-				if (m_a->hasNext()) { m_b = m_f( *(m_a->next()) ); }
+				if (m_a->hasNext()) { m_b = m_f( m_a->next() ); }
 				return m_b->next();
 			}
 			bool hasNext(void) { return m_a->hasNext() && m_b->hasNext(); }
@@ -69,17 +70,44 @@ namespace mimi
 			mplus(std::unique_ptr<iterator<A>> a, std::unique_ptr<iterator<A>> b) :
 				m_a(std::move(a)), m_b(std::move(b)) { }
 
-			std::unique_ptr<A> next(void)
+			A next(void)
 			{
 				if (m_a->hasNext()) { return m_a->next(); }
-				else if (m_b->hasNext()) { return m_b->next(); }
-				else { return nullptr; }
+				else { return m_b->next(); }
 			}
 			bool hasNext(void) { return m_a->hasNext() || m_b->hasNext(); }
 
 		private:
 			std::unique_ptr<iterator<A>> m_a, m_b;
 	};
+
+	template <typename A>
+	std::unique_ptr<iterator<A>> make_mimi(void)
+	{
+		return std::make_unique<mzero<A>>();
+	}
+
+	template <typename A>
+	std::unique_ptr<iterator<A>> make_mimi(A item)
+	{
+		return std::make_unique<unit<A>>(item);
+	}
+
+	template <typename A, typename B>
+	std::unique_ptr<iterator<A>> make_mimi(
+			std::unique_ptr<iterator<A>> a,
+			std::function<std::unique_ptr<iterator<B>> (A)> f)
+	{
+		return std::make_unique<bind<A, B>>(std::move(a), f);
+	}
+
+	template <typename A>
+	std::unique_ptr<iterator<A>> make_mimi(
+		std::unique_ptr<iterator<A>> a,
+		std::unique_ptr<iterator<A>> b)
+	{
+		return std::make_unique<mplus<A>>(std::move(a), std::move(b));
+	}
 };
 
 #endif
