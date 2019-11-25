22a5d3d2da17aabee3493daa13b8a0baef778343
Nicholas Warfield <warfield9000@gmail.com>
2019-11-25 15:25:52 -0800

    MVP completed the monad is in a state where I can submit it, but it still could use improvement (lots).

--- mimi.hpp
+++ mimi.hpp
@@ -11,7 +11,7 @@ namespace mimi
 	class iterator
 	{
 		public:
-			virtual ~iterator(void);
+			virtual ~iterator(void) { }
 			virtual std::optional<A> next(void) = 0;
 			virtual bool hasNext(void) = 0;
 	};
@@ -45,20 +45,20 @@ namespace mimi
 	class bind : public iterator<A>
 	{
 		public:
-			bind(iterator<A>& a,
+			bind(std::unique_ptr<iterator<A>> a,
 					std::function<std::unique_ptr<iterator<B>> (A)> f) :
-				m_a(std::make_unique<iterator<A>>(a)),
+				m_a(std::move(a)),
+				m_b(std::make_unique<mzero<B>>()),
 				m_func(f)
-			{
-			}
+			{ }
 
 			std::optional<A> next(void)
 			{
 				if (m_b->hasNext()) { return m_b->next(); }
-				if (m_a->hasNext()) { m_b = m_f( m_a->next() ); }
+				if (m_a->hasNext()) { m_b = m_func( m_a->next().value() ); }
 				return m_b->next();
 			}
-			bool hasNext(void) { return m_a.hasNext() && m_b.hasNext(); }
+			bool hasNext(void) { return m_a->hasNext() || m_b->hasNext(); }
 
 		private:
 			std::unique_ptr<iterator<A>> m_a;
@@ -70,17 +70,12 @@ namespace mimi
 	class mplus : public iterator<A>
 	{
 		public:
-			mplus(iterator<A>& a, iterator<A>& b) :
-				m_a(std::make_unique<iterator<A>>(a)),
-				m_b(std::make_unique<iterator<A>>(b))
-			{ }
+			mplus(std::unique_ptr<iterator<A>> a, std::unique_ptr<iterator<A>> b)
+				: m_a(std::move(a)), m_b(std::move(b)) { }
 
 			std::optional<A> next(void)
 			{
-				bool aNext = m_a->hasNext();
-				auto a = m_a->next();
-				auto b = m_b->next();
-				return aNext ? a : b;
+				return m_a->hasNext() ? m_a->next() : m_b->next();
 			}
 			bool hasNext(void) { return m_a->hasNext() || m_b->hasNext(); }
 
@@ -102,16 +97,16 @@ namespace mimi
 
 	template <typename A, typename B>
 	auto make_mimi(
-			iterator<A>& a,
+			std::unique_ptr<iterator<A>> a,
 			std::function<std::unique_ptr<iterator<B>> (A)> f)
 	{
-		return std::make_unique<bind<A, B>>(a, f);
+		return std::make_unique<bind<A, B>>(std::move(a), f);
 	}
 
 	template <typename A>
-	auto make_mimi(iterator<A>& a, iterator<A>& b)
+	auto make_mimi(std::unique_ptr<iterator<A>> a, std::unique_ptr<iterator<A>> b)
 	{
-		return std::make_unique<mplus<A>>(a, b);
+		return std::make_unique<mplus<A>>(std::move(a), std::move(b));
 	}
 };
 
