81a3ce2db548cbde84a7f4e6d299372a7ae8b81c
Nicholas Warfield <warfield9000@gmail.com>
2019-11-25 19:12:14 -0800

    Changed unique ptr to raw ptr Not sure how I feel about this. It cleans up the interface a bunch and makes the code much more readable. And it lets me pass by reference now. But I'm not convinced that it is better or that there aren't bugs somewhere. Need to llok into it more and test it.

--- mimi.hpp
+++ mimi.hpp
@@ -1,6 +1,7 @@
 #ifndef MIMI_HPP_INCLUDED
 #define MIMI_HPP_INCLUDED
 
+#include <iostream>
 #include <functional>
 #include <memory>
 #include <optional>
@@ -15,6 +16,9 @@ namespace mimi
 			virtual std::optional<A> next(void) = 0;
 			virtual bool hasNext(void) = 0;
 	};
+
+	template <typename T>
+	using itt = std::unique_ptr<iterator<T>>;
 
 	template <typename A>
 	class mzero : public iterator<A>
@@ -45,10 +49,9 @@ namespace mimi
 	class bind : public iterator<B>
 	{
 		public:
-			bind(std::unique_ptr<iterator<A>> a,
-					std::function<std::unique_ptr<iterator<B>> (A)> f) :
-				m_a(std::move(a)),
-				m_b(std::make_unique<mzero<B>>()),
+			bind(iterator<A>& a, std::function<iterator<B>* (A)> f) :
+				m_a(&a),
+				m_b(new mzero<B>()),
 				m_func(f)
 			{ }
 
@@ -61,17 +64,16 @@ namespace mimi
 			bool hasNext(void) { return m_a->hasNext() || m_b->hasNext(); }
 
 		private:
-			std::unique_ptr<iterator<A>> m_a;
-			std::unique_ptr<iterator<B>> m_b;
-			std::function<std::unique_ptr<iterator<B>> (A)> m_func;
+			iterator<A> *m_a;
+			iterator<B> *m_b;
+			std::function<iterator<B>* (A)> m_func;
 	};
 
 	template <typename A>
 	class mplus : public iterator<A>
 	{
 		public:
-			mplus(std::unique_ptr<iterator<A>> a, std::unique_ptr<iterator<A>> b)
-				: m_a(std::move(a)), m_b(std::move(b)) { }
+			mplus(iterator<A>& a, iterator<A>& b) : m_a(&a), m_b(&b) { }
 
 			std::optional<A> next(void)
 			{
@@ -80,34 +82,34 @@ namespace mimi
 			bool hasNext(void) { return m_a->hasNext() || m_b->hasNext(); }
 
 		private:
-			std::unique_ptr<iterator<A>> m_a, m_b;
+			iterator<A> *m_a, *m_b;
 	};
 
-	template <typename A>
-	auto make_mimi(void)
-	{
-		return std::make_unique<mzero<A>>();
-	}
-
-	template <typename A>
-	auto make_mimi(A item)
-	{
-		return std::make_unique<unit<A>>(item);
-	}
-
-	template <typename A, typename B>
-	auto make_mimi(
-			std::unique_ptr<iterator<A>> a,
-			std::function<std::unique_ptr<iterator<B>> (A)> f)
-	{
-		return std::make_unique<bind<A, B>>(std::move(a), f);
-	}
-
-	template <typename A>
-	auto make_mimi(std::unique_ptr<iterator<A>> a, std::unique_ptr<iterator<A>> b)
-	{
-		return std::make_unique<mplus<A>>(std::move(a), std::move(b));
-	}
+//	template <typename A>
+//	auto make_mimi(void)
+//	{
+//		return std::make_unique<mzero<A>>();
+//	}
+//
+//	template <typename A>
+//	auto make_mimi(A item)
+//	{
+//		return std::make_unique<unit<A>>(item);
+//	}
+//
+//	template <typename A, typename B>
+//	auto make_mimi(
+//			std::unique_ptr<iterator<A>> a,
+//			std::function<std::unique_ptr<iterator<B>> (A)> f)
+//	{
+//		return std::make_unique<bind<A, B>>(std::move(a), f);
+//	}
+//
+//	template <typename A>
+//	auto make_mimi(std::unique_ptr<iterator<A>> a, std::unique_ptr<iterator<A>> b)
+//	{
+//		return std::make_unique<mplus<A>>(std::move(a), std::move(b));
+//	}
 };
 
 #endif
